20100b31a4b490c8343c1c13882dd5ac97569340
Jong Hoon Lee <whdgns0665@Jongs-Air.fios-router.home>
2019-08-06 02:13:46 -0400

    Adding inverse bwt

--- js/bwt.js
+++ js/bwt.js
@@ -1,57 +1,84 @@
+
+
 function bwt() {
 	clearContent();
+	document.getElementById("inverse").style.display = "none";
 	var original = document.getElementById("originalValue").value;
 
-	document.getElementById("step1").style.display = "block";
+
+	//var pattern = /^[a-z\d\s]+$/i;
+	//if(!original.match(pattern)) {
+	if (original.includes("$")) {
+		document.getElementById("error").style.display = "block";
+		return;
+	} else {
+		document.getElementById("error").style.display = "none";
+	}
+
+	document.getElementById("forward").style.display = "block";
 	original = original + "$";
 	cyclicStrings = generateShifts(original);
 
-	document.getElementById("step2").style.display = "block";
-	sortedStrings = sortShifts(cyclicStrings);
+	sortedStrings = sortShifts(cyclicStrings, original);
 
-	document.getElementById("step3").style.display = "block";
 	BWTOutput = generateBWTOutput(sortedStrings);
 }
 
 function clearContent() {
-	document.getElementById("step1").style.display = "none";
-	document.getElementById("step2").style.display = "none";
-	document.getElementById("step3").style.display = "none";
+	document.getElementById("forward").style.display = "none";
+	document.getElementById("ibwt").style.display= "block";
 
 	document.getElementById("rotations").innerHTML = "";
 	document.getElementById("sortedRotations").innerHTML = "";
 	document.getElementById("bwtOutput").innerHTML = "";
-
-	document.getElementById("rotations").style.display = "none";
-	document.getElementById("sortedRotations").style.display = "none";
-	document.getElementById("bwtOutput").style.display = "none";
+	document.getElementById("step2substr").innerHTML = "";
+	document.getElementById("orig_index").innerHTML = "";
+	document.getElementById("sortedbwt").innerHTML = "";
+	document.getElementById("step5substr").innerHTML = "";
+	document.getElementById("iorig_index").innerHTML = "";
+	document.getElementById("lists").innerHTML = "";
+	document.getElementById("step7substr").innerHTML = "";
 }
 
 function generateShifts(original) {
-	console.log(original);
 	var len = original.length;
 	var curString = original;
 	var cyclicStrings = [];
 	for (var i = 0; i < len; i++) {
 		cyclicStrings.push(curString);
-		document.getElementById("rotations").innerHTML += "• " +  curString + "<br />"
+		document.getElementById("rotations").innerHTML += curString.split(' ').join('&nbsp;') + " [" + i +  "]" + "<br />";
 		curString = curString[len - 1] + curString.slice(0, len - 1);
 	}
-	document.getElementById("rotations").style.display = "block";
-	console.log(cyclicStrings);
 	return cyclicStrings;
 	
 }
 
-function sortShifts(cyclicStrings) {
-	sortedStrings = cyclicStrings.sort((a, b) => a.localeCompare(b)); 
-	console.log(sortedStrings);
+function sortShifts(cyclicStrings, original) {
+	sortedStrings = cyclicStrings.sort(function(a, b) { 
+		if (a[0] == "$")
+			return -1;
+		if (b[0] == "$")
+			return 1;
+		return a.localeCompare(b, {sensitivity: 'case'});
+	});
+	var orig_index = 0;
 
 	var len = sortedStrings.length;
 	for (var i = 0; i < len; i++) {
-		document.getElementById("sortedRotations").innerHTML += "• " + sortedStrings[i] + "<br />"
+		if (sortedStrings[i] == original) {
+			document.getElementById("sortedRotations").innerHTML += "<b>" + sortedStrings[i].split(' ').join('&nbsp;') + " [" + i +  "]" + "</b><br />";
+			orig_index = i;
+			continue;
+		}
+		document.getElementById("sortedRotations").innerHTML += sortedStrings[i].split(' ').join('&nbsp;') + " [" + i +  "]" + "<br />";
 	}
-	document.getElementById("sortedRotations").style.display = "block";
+
+	if (original.length < 40) {
+		document.getElementById("step2substr").innerHTML = original;
+	} else {
+		document.getElementById("step2substr").innerHTML = original.substring(0, 35) + "...";
+	}
+	document.getElementById("orig_index").innerHTML = orig_index;
 
 	return sortedStrings;
 }
@@ -62,9 +89,6 @@ function generateBWTOutput(sortedStrings) {
 	for (var i = 0; i < len; i++) {
 		BWTOutput += sortedStrings[i][len - 1];
 	}
-	console.log(BWTOutput);
-
-	document.getElementById("bwtOutput").style.display = "block";
 
 	document.getElementById("bwtOutput").textContent = BWTOutput;
 	return BWTOutput;
---
+++ js/ibwt.js
@@ -0,0 +1,77 @@
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
+
+function indexOf(str, ch) { 
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] == ch)
+			return i;
+	}
+}
+
+function ibwt() {
+	document.getElementById("ibwt").style.display= "none";
+	document.getElementById("inverse").style.display = "block";
+
+	var bwtOutput = document.getElementById("bwtOutput").textContent;
+	const bwt = bwtOutput;
+
+	if (bwtOutput.length < 40) {
+		document.getElementById("step5substr").innerHTML = bwtOutput;
+	} else {
+		document.getElementById("step5substr").innerHTML = bwtOutput.substring(0, 35) + "...";
+	}
+
+
+	var original = document.getElementById("originalValue").value;
+	original = original + "$";
+	originalArray = original.split('');
+
+	var sortedString = originalArray.sort(function(a, b) { 
+		if (a[0] == "$")
+			return -1;
+		if (b[0] == "$")
+			return 1;
+		return a.localeCompare(b, {sensitivity: 'case'});
+	});
+
+	sortedString = sortedString.join('');
+
+	document.getElementById("sortedbwt").innerHTML = sortedString;
+
+	var orig_index = document.getElementById("orig_index").innerHTML;
+	document.getElementById("iorig_index").innerHTML = orig_index;
+
+	var prev = "$"
+	var str = bwtOutput;
+	var left_index = [];
+	left_index.push(parseInt(orig_index));
+	document.getElementById("lists").innerHTML = prev + ": [" + orig_index;
+	for (var i = 1; i < sortedString.length; i++) {
+		if (sortedString[i] != prev) {
+			document.getElementById("lists").innerHTML += "]<br /><b>" + sortedString[i] + "</b>: [";
+		} 
+		var j = str.indexOf(sortedString[i]);
+		document.getElementById("lists").innerHTML += " " + j + " "
+		//var j = indexOf(str, sortedString[i]);
+		left_index.push(j);
+
+		str = str.replaceAt(j, '$');
+
+		prev = sortedString[i];
+	}
+	document.getElementById("lists").innerHTML += "]"
+
+	var x = parseInt(orig_index);
+	var finalOutput = [];
+	for (var i = 0; i < left_index.length; i++) {
+		x = left_index[x];
+
+		finalOutput.push(bwt[x]);
+	}
+
+	finalOutput = finalOutput.join('');
+
+	document.getElementById("step7substr").innerHTML = original;
+
+}
